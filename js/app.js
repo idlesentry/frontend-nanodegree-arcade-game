7b0aedf1e17ea45c35a001dff0e3c6e3feeb98f8
idlesentry <idlesentry@gmail.com>
2015-11-24 20:34:59 -0800

    removed comments

--- js/app.js
+++ js/app.js
@@ -1,36 +1,19 @@
 var enemy = function(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.x = x;
     this.y = y;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-    //this.x = this.x + Math.random() * 500 * dt;
-    this.x = this.x + (Math.floor((Math.random() * 10)));
+    this.x = this.x + (Math.floor((Math.random() * 500))) * dt;
     if (this.x > 500) {
         this.x = 0; 
     }
 };
 
-// Draw the enemy on the screen, required method for game
 enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 var player = function(x,y) {
     this.x = x;
@@ -89,7 +72,6 @@ player.prototype.update = function(dt) {
     }
 };
 
-// Draw the enemy on the screen, required method for game
 player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -100,16 +82,8 @@ var enemy1 = new enemy(-100,50);
 var enemy2 = new enemy(-100,140);
 var enemy3 = new enemy(-100,225);
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
 var allEnemies = [enemy1,enemy2,enemy3];
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
